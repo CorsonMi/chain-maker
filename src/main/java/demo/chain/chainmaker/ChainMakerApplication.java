f048fdec4b5e44d48720a0eba5c35ea81b16fefb
kesen.cheng <kesen.cheng@happyelements.com>
2023-02-22 17:55:46 +0800

    初始化

---
+++ src/main/java/demo/chain/chainmaker/ChainMakerApplication.java
@@ -0,0 +1,12 @@
+package demo.chain.chainmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainMakerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChainMakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/demo/chain/chainmaker/ChainMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.chain.chainmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChainMakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
